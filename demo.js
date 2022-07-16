3210c09ca65f4383d456fe0002b18ddf20c88231
izumitomo <luckystar.tomosuke@gmail.com>
2022-07-16 17:48:28 +0900

    demo

---
+++ demo.js
@@ -0,0 +1,21 @@
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri =
+	"mongodb+srv://izumi:sax6vYiJy6BpTDs@cluster0.ceckb.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+	serverApi: ServerApiVersion.v1,
+});
+client.connect((err) => {
+	const collection = client.db("test").collection("devices");
+	// perform actions on the collection object
+  console.log("Connected successfully to server");
+
+  const documents = [{ a: 1 }, { a: 2 }, { a: 3 }];
+
+  collection.insertMany(documents, (err, result) => {
+    console.log("Inserted 3 documents into the collection");
+    console.log(result);
+    client.close();
+  });
+});
